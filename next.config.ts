430b107c30b02c12d64d54d3be3cbb3e1eea8377
yazanrababah16-pixel <yazanrababah16@gmail.com>
2026-07-18 18:25:49 +0300

    fix: resolve toast import error

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,4 @@ export default withSentryConfig(nextConfig, {
   silent: !process.env.CI,
   widenClientFileUpload: true,
   tunnelRoute: "/monitoring",
-  disableLogger: true,
-  automaticVercelMonitors: true,
 });
